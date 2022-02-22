a97c42c16177099bbfa9b91c2a5e204b707e05f1
Rizco Renova <95638757+renova213@users.noreply.github.com>
2022-02-22 10:43:59 +0700

    added dart file

---
+++ FizzBuzz.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+
+void main() {
+  stdout.write("Input length : ");
+  int x = int.parse(stdin.readLineSync().toString());
+
+  for (var i = 1; i <= x; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+      print("FizzBuzz");
+    } else if (i % 3 == 0) {
+      print("Fizz");
+    } else if (i % 5 == 0) {
+      print("Buzz");
+    } else {
+      print(i);
+    }
+  }
+}
